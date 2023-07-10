8d85795701735e1e1d004c66f0e1f3590c2b4b46
Eriq606 <erohweltall@gmail.com>
2023-07-10 15:17:22 +0300

    insert objectif

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'session');
 
 /*
 | -------------------------------------------------------------------
@@ -133,5 +133,8 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array(
-    'LoginModel' => 'LoginModel'
+    'LoginModel' => 'LoginModel',
+    'ObjectifModel' => 'ObjectifModel',
+    'ProfileModel' => 'ProfileModel',
+    'RegimeModel' => 'RegimeModel'
 );
