3721b444eeeeb50298fc9dc285eb50be9b9f5a17
Rebeka-1892 <etu1892@gmail.com>
2023-07-10 17:27:51 +0200

    insert user -> utilisateur

--- tools/SQL/DATA.sql
+++ tools/SQL/DATA.sql
@@ -1,6 +1,6 @@
-insert into user values (null, 'John', '1234', 1);
-insert into user values (null, 'Alice', '1234', 0);
-insert into user values (null, 'Luc', '1234', 0);
+insert into utilisateur values (null, 'John', '1234', 1);
+insert into utilisateur values (null, 'Alice', '1234', 0);
+insert into utilisateur values (null, 'Luc', '1234', 0);
 
 insert into genre values (1, 'Homme');
 insert into genre values (2, 'Femme');
