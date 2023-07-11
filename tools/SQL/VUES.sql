94ce802758dd6a34183ade9c3ecb54c4e061a252
Rebeka-1892 <etu1892@gmail.com>
2023-07-11 11:19:07 +0200

    duree

--- tools/SQL/VUES.sql
+++ tools/SQL/VUES.sql
@@ -1,5 +1,5 @@
 CREATE or replace view v_regimeplat as
-    select r.idregime, reg.descriRegime, p.descriPlat, r.quantite, u.descriUnite, p.pu, reg.poids from regimePlat r 
+    select r.idregime, reg.descriRegime, p.descriPlat, r.quantite, u.descriUnite, p.pu, reg.poids, reg.duree from regimePlat r 
     join plat p on r.idplat=p.idplat join unite u on u.idunite=p.idunite join regime reg on reg.idregime=r.idregime;
 
 
