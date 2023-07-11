12802b0107e1a6f33c6fc4a3ba5dbec28aff5456
Rebeka-1892 <etu1892@gmail.com>
2023-07-11 13:11:10 +0200

    plat

--- application/models/PlatModel.php
+++ application/models/PlatModel.php
@@ -4,11 +4,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class PlatModel extends CI_Model {
     public function getPlatById($idplat){
         $this->db->select("*");
-        $this->db->from("plat");
+        $this->db->from("v_plat");
         $this->db->where("idplat", $idplat);
         $result=$this->db->get();
         $result=$result->result();
         return $result[0];
+    }
+
+    public function getUnite(){
+        $this->db->select("*");
+        $this->db->from("unite");
+        $query = $this->db->get();
+        $resultat = $query->result();
+
+        if(count($resultat) > 0) {
+            return $resultat;
+        }
     }
 
     public function savePlat($descriPLat, $pu, $idunite){
--- application/views/backOffice/plat/updatePlat.php
+++ application/views/backOffice/plat/updatePlat.php
@@ -12,23 +12,26 @@ $this->load->view('templates/leftAdmin');
                     <div class="card-body">
                         <h4 class="card-title">Modifier un plat</h4>
                         <p class="card-description"> Remplissez le formulaire </p>
-                        <form class="forms-sample">
+                        <form class="forms-sample" method="post" action="<?php echo site_url('plat/update'); ?>">
                             <div class="form-group">
                                 <div class="form-group">
+                                    <input name="idplat" type="hidden" value="<?php echo($plat->idplat); ?>">
                                     <label for="description">description</label>
-                                    <input type="text" class="form-control" id="description" placeholder="description du plat"
+                                    <input name="descriPLat" type="text" class="form-control" id="description" placeholder="description du plat"
                                     value="<?php echo($plat->descriPlat); ?>"  required>
                                 </div>
                                 <div class="form-group">
                                     <label for="prixUnitaire">Prix unitaire</label>
-                                    <input type="number" class="form-control" id="prixUnitaire" placeholder="prix unitaire du plat"
+                                    <input name="pu" type="number" class="form-control" id="prixUnitaire" placeholder="prix unitaire du plat"
                                     value="<?php echo($plat->pu); ?>" step="0.1"  required>
                                 </div>
                                 <div class="form-group">
                                     <label for="unite" style="margin-right:15px;">Unite</label>
-                                    <select name="unite" id="unite">
-                                        <option value="1">Kg</option>
-                                        <option value="1">Kg</option>
+                                    <select name="idunite" id="unite" >
+                                        <option value="<?php echo($plat->idunite); ?>"><?php echo($plat->descriUnite); ?></option>
+                                        <?php foreach ($unites as $unite) { ?>
+                                            <option value="<?php echo($unite->idunite); ?>"><?php echo($unite->descriUnite); ?></option>
+                                        <?php } ?>
                                     </select>
                                 </div>
                                 <button type="submit" class="btn btn-gradient-primary me-2">Modifier</button>
