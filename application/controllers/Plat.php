a2f5bdeb69e0f9ee50d005fe69a836e5e276e358
Yoan <yoan.rab@gmail.com>
2023-07-11 09:55:14 +0200

    controllers

---
+++ application/controllers/Plat.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Plat extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+		
+	}	
+
+    public function delete($idPlat)
+	{
+		$user=$this->session->user;
+		$data['user']=$user;
+		redirect('plat/liste');
+	}
+	
+	public function update($idPlat)
+	{
+		$user=$this->session->user;
+		$data['user']=$user;
+		$this->load->view('backOffice/plat/updatePlat', $data);
+	}
+    
+    public function create()
+	{
+		$data['user']=$this->session->user;
+		$this->load->view('backOffice/plat/createPlat', $data);
+		
+	}
+    
+    public function liste()
+	{
+		$user=$this->session->user;
+		$data['user']=$user;
+		$this->load->view('backOffice/plat/listePlats', $data);
+	}
+}
